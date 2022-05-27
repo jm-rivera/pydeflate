80e2ec1631405ecaf057b0b812937688992d5a5f
Jorge Rivera <jorge.rivera@one.org>
2022-05-27 09:59:06 -0600

    format deflate.py and annotate

--- pydeflate/deflate/deflate.py
+++ pydeflate/deflate/deflate.py
@@ -5,15 +5,12 @@ from typing import Optional, Callable, Union
 
 import pandas as pd
 
-from pydeflate.get_data.data import Data
-from pydeflate.utils import check_year_as_number
-
 from pydeflate.deflate.deflator import Deflator
+from pydeflate.get_data.data import Data
 from pydeflate.get_data.imf_data import IMF
 from pydeflate.get_data.oecd_data import OECD, OECD_XE
 from pydeflate.get_data.wb_data import WB, WB_XE
-from pydeflate.utils import to_iso3, oecd_codes
-
+from pydeflate.utils import check_year_as_number, to_iso3, oecd_codes
 
 DEFLATORS = {
     "oecd_dac": OECD,
@@ -117,47 +114,55 @@ def deflate(
         are no deflators will be returned as null values.
 
     """
-    # Backwards compatibility
+    # Backwards compatibility: if the iso_column parameter is used, reassign it to
+    # the ID column
     if iso_column is not None:
         id_column = iso_column
 
-    # Reverse check
+    # Check if the reverse option (i.e. from constant to current is used). If so,
+    # then also switch the target and source currencies.
     if reverse:
         sc = source_currency
         source_currency = target_currency
         target_currency = sc
 
-    # keep track of original columns
+    # keep track of original columns. This is so that the same order and columns can be
+    # preserved.
     if target_col not in df.columns:
         cols = [*df.columns, target_col]
     else:
         cols = df.columns
 
+    # Check if the source specified is part of the available deflators source
     if source not in DEFLATORS.keys():
         raise ValueError(f'source "{source}" not valid')
 
     # check if date format matches
     df, year_as_number = check_year_as_number(df, date_column)
 
-    # Create ID col
+    # Create ID col.  By default, if a country does not have a DAC deflator, the
+    # DAC,Total deflator is used.
     if id_type == "DAC":
-        df["id_"] = df[id_column].map(oecd_codes).fillna('DAC')
+        df["id_"] = df[id_column].map(oecd_codes).fillna("DAC")
     else:
         df = df.pipe(
             to_iso3, codes_col=id_column, target_col="id_", src_classification=id_type
         )
 
-    # Define exchange and price data objects
+    # Create exchange and price data objects. The specific objects created depend on the
+    # source and/or method specified
     xe: Data = EXCHANGE[source]()
     price: Data = DEFLATORS[source](method=method)
 
-    # Get deflator functions
+    # Get deflator functions. The Deflator object takes in functions (as Callables)
+    # instead of taking in the actual data.
     price_dfl: Callable = price.get_deflator
     x_dfl: Callable = xe.get_deflator
 
-    # Get currency exchange data frame
+    # Get currency exchange DataFrame. This is based on the target currency
     x_rate: pd.DataFrame = xe.get_data(currency_iso=target_currency).copy()
 
+    # Create a Deflator object and get the deflator DataFrame.
     deflator = Deflator(
         base_year=base_year,
         xe_deflator=x_dfl,
@@ -167,6 +172,7 @@ def deflate(
         target_currency=target_currency,
     ).deflator()
 
+    # Merge the original data with the deflator DataFrame
     df = df.merge(
         deflator,
         left_on=["id_", date_column],
@@ -175,14 +181,17 @@ def deflate(
         suffixes=("", "_"),
     )
 
+    # If the reverse option is used, multiply the data. Else divide.
     if reverse:
         df[target_col] = df[source_col] * (df.deflator / 100)
 
     else:
         df[target_col] = df[source_col] / (df.deflator / 100)
 
+    # Keep only the columns present in the original DataFrame (including order)
     df = df.filter(cols, axis=1)
 
+    # If the year is passed as a number, convert it back to a number.
     if year_as_number:
         df[date_column] = df[date_column].dt.year
 
