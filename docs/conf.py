fbe51b4fc84fe0e21ecc36357d8cdaacfa0c710c
Jorge Rivera <jorge.rivera@one.org>
2022-07-26 22:56:11 +0200

    Bump version: 1.1.6 → 1.1.7

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-__version__ = '1.1.6'
+__version__ = '1.1.7'
 
 project = 'pydeflate'
 copyright = '2022, Jorge Rivera'
