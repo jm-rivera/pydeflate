48c5179d1a89e7a3a3e691df3b2d1d4aead640ca
Jorge Rivera <jorge.rivera@one.org>
2024-11-16 23:24:26 +0100

    get last element as date

--- pydeflate/sources/common.py
+++ pydeflate/sources/common.py
@@ -21,7 +21,7 @@ def check_file_age(file: Path) -> int:
     """
     current_date = datetime.today()
     # Extract date from the filename (format: weo_YYYY-MM-DD.parquet)
-    file_date = datetime.strptime(file.stem.split("_")[1], "%Y-%m-%d")
+    file_date = datetime.strptime(file.stem.split("_")[-1], "%Y-%m-%d")
 
     # Return the difference in days between today and the file's date
     return (current_date - file_date).days
